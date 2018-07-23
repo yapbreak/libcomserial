29ec8e9738e5f8e6e48fc58b38738eae5a3a6925
Adrien Oliva <olivaa+gitlab@yapbreak.fr>
2018-07-23 10:40:53 +0200

    feat(logger): Use latest yaplog version (2.0.0)

--- src/__init__.cpp
+++ src/__init__.cpp
@@ -3,14 +3,11 @@
 // cppcheck-suppress unusedFunction
 __attribute__((constructor)) static void libcomserial_static_init()
 {
-#if HAVE_YAPLOG
-    logger::setDestinationVariable("CSER_LOGDESTINATION");
-    logger::setLevelVariable("CSER_LOGLEVEL");
-#endif
     TLOG() << "Load comserial library";
 }
 
 // cppcheck-suppress unusedFunction
 __attribute__((destructor)) static void libcomserial_static_deinit()
 {
+    TLOG() << "Unload comserial library";
 }
